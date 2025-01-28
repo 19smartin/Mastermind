41b2f762a7c93c920a54e5586abeed2e127d05f0
Sam Martin <19smartin@thelangton.org.uk>
2025-01-28 23:01:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 #main
 import pygame
-from settings import *
-from sprites import *
+import random
 
-class Game:
+pygame.init()
+
+WIDTH = 450
+HEIGHT = 800
+timer = pygame.time.Clock()
+fps = 60
+screen = pygame.display.set_mode([WIDTH,HEIGHT])
+pygame.display.set_caption('Mastermind!')
+font = pygame.font.Font('freesanabold.ttf', 18)
+# game variables
+background = int(.125*255), int(.165*255), int(.267*255)
+
+
+run = True
+while run:
+    timer.tick(fps)
+    screen.fill(background)
     
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.flip()
+pygame.quit()
